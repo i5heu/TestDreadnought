cad7838afccad51688693b3588916b43638d180f
Mia <i5heu@i5h.eu>
2024-07-14 13:00:17 +0200

    Remove unnecessary test case and result file

--- test_example/testNameFoo/justATestCase.js
+++
@@ -1,8 +0,0 @@
-Settings = globalSettings;
-
-body = "this is the body yada yada";
-data = { body: body };
-
-result = Post("/RAKBtY2U7CrEdANof0fu", data);
-ResultIsLikeFile(result.body, "./result.txt");
-ThisIsTest();
